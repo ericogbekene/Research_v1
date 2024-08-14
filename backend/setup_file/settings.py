234e64264b88531aa378c1e011e383ad31bb25f1
cysof <cp4reallife@yahoo.com>
2024-08-14 22:34:39 +0100

    added corsheader in the middleware

--- backend/setup_file/settings.py
+++ backend/setup_file/settings.py
@@ -41,7 +41,7 @@ CORS_ALLOW_ALL_ORIGINS = []
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    ''
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
