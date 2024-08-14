dbff89158757272ab5c03d3f19436fba0d1e5506
cysof <cp4reallife@yahoo.com>
2024-08-14 22:41:17 +0100

    fix cros headers allow origin

--- backend/setup_file/settings.py
+++ backend/setup_file/settings.py
@@ -36,7 +36,13 @@ INSTALLED_APPS = [
     'researchpro',
 
 ]
-CORS_ALLOW_ALL_ORIGINS = []
+
+CORS_ALLOWED_ORIGINS = [
+   # "https://example.com",
+   # "https://sub.example.com",
+   # "http://localhost:8080",
+   # "http://127.0.0.1:9000",
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
