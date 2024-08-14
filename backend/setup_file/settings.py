c10bb075078888b47e63316ff1b791fe3cadcdd8
cysof <cp4reallife@yahoo.com>
2024-08-14 18:53:44 +0100

    created django app call researchpro

--- backend/setup_file/settings.py
+++ backend/setup_file/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # framework
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
+
+    # Local apps
+    'researchpro',
+
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
