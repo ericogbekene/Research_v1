0cdf757fe26a943ad3fc719c3585fb7bedc5cb70
cysof <cp4reallife@yahoo.com>
2024-08-18 16:55:18 +0100

    db

--- backend/setup_file/settings.py
+++ backend/setup_file/settings.py
@@ -87,18 +87,15 @@ WSGI_APPLICATION = 'setup_file.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-  # }
-#}
-
-# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
-DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL_'), conn_max_age=600)
+
+
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL_'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
